fb1d009607aaa3df690229dfcacebf3640422a1e
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-08-02 00:46:45 +0300

    Add routes and components folder

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <>Hello NEXT</>;
+  return (
+    <>
+      <h1>Hello NEXT</h1>
+      <a href="/cabins">cabins</a>
+      <Link href="/cabins">Cabins</Link>
+      <Link href="/account">Account</Link>
+      <Link href="/about">About</Link>
+    </>
+  );
 }
