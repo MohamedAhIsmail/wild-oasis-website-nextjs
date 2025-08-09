0728831936b71d1198ef23696d3ac53836801aa4
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-08-10 01:57:42 +0300

    Add fav icon and ggoe font to the App

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,25 @@
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./_styles/globals.css";
-
+import { Josefin_Sans } from "next/font/google";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import Logo from "./components/Logo";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -29,7 +34,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-50 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-50 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
